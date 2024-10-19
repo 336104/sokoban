73c06b343e0920b8bc1d4090419c37164c2e00fd
lmx <3361040032@qq.com>
2024-10-19 11:19:33 +0800

    add tests

--- src/SokobanTest.java
+++ src/SokobanTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.stream.IntStream;
 
 public class SokobanTest {
     private SokobanGame sokobanGame;
@@ -33,59 +34,65 @@ public class SokobanTest {
      * 所以实际selectLevel传入的参数与实际输入的关卡数是差1的，所以传入的参数应该+1才是实际选择的关卡数目
      */
 
-   @Test
+    @Test
     public void testSelectLevel1() {
         // 测试选择有效关卡
         sokobanGame.selectLevel(-1); // 选择第0关
-        Assert.assertEquals(0, sokobanGame.iCurLevel+1); // 超出范围无效
+        Assert.assertEquals(0, sokobanGame.iCurLevel + 1); // 超出范围无效
     }
+
     @Test
     public void testSelectLevel2() {
         // 测试选择有效关卡
         sokobanGame.selectLevel(0); // 选择第1关
-        Assert.assertEquals(1, sokobanGame.iCurLevel+1); // 预期关卡应该是1
+        Assert.assertEquals(1, sokobanGame.iCurLevel + 1); // 预期关卡应该是1
     }
+
     @Test
     public void testSelectLevel3() {
         // 测试选择有效关卡
         sokobanGame.selectLevel(1); // 选择第2关
-        Assert.assertEquals(2, sokobanGame.iCurLevel+1); // 预期关卡应该是2
+        Assert.assertEquals(2, sokobanGame.iCurLevel + 1); // 预期关卡应该是2
     }
+
     @Test
     public void testSelectLevel4() {
         // 测试选择有效关卡
         sokobanGame.selectLevel(49); // 选择第50关
-        Assert.assertEquals(50, sokobanGame.iCurLevel+1); // 预期关卡应该是50
+        Assert.assertEquals(50, sokobanGame.iCurLevel + 1); // 预期关卡应该是50
     }
+
     @Test
     public void testSelectLevel5() {
         // 测试选择有效关卡
         sokobanGame.selectLevel(98); // 选择第99关
-        Assert.assertEquals(99, sokobanGame.iCurLevel+1); // 预期关卡应该是99
+        Assert.assertEquals(99, sokobanGame.iCurLevel + 1); // 预期关卡应该是99
     }
+
     @Test
     public void testSelectLevel6() {
         // 测试选择有效关卡
         sokobanGame.selectLevel(99); // 选择第100关
-        Assert.assertEquals(100, sokobanGame.iCurLevel+1); // 预期关卡应该是100
+        Assert.assertEquals(100, sokobanGame.iCurLevel + 1); // 预期关卡应该是100
     }
 
     @Test
     public void testSelectLevel7() {
         // 测试选择有效关卡
         sokobanGame.selectLevel(100); // 选择第101关
-        Assert.assertEquals(101, sokobanGame.iCurLevel+1); // 超出范围无效
+        Assert.assertEquals(101, sokobanGame.iCurLevel + 1); // 超出范围无效
     }
+
     @Test
     public void testSelectLevel8() {
         // 测试选择有效关卡
-        sokobanGame.selectLevel((int)1.5); // 选择第2关,注意因为如果有小数点的话，这个处理部分应该是在输入读取的时候处理，再传参数给select函数
-        Assert.assertEquals(2, sokobanGame.iCurLevel+1); //预期关卡是2
+        sokobanGame.selectLevel((int) 1.5); // 选择第2关,注意因为如果有小数点的话，这个处理部分应该是在输入读取的时候处理，再传参数给select函数
+        Assert.assertEquals(2, sokobanGame.iCurLevel + 1); //预期关卡是2
     }
 
-    
+    //    测试玩家能否正常移到到空地
     @Test
-    public void testPlayerMove28() {
+    public void testPlayerMoveSpace() {
         int[][] expectMap = new int[][]{
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -109,9 +116,9 @@ public class SokobanTest {
         Assert.assertEquals(1, sokobanGame.moveTimes);
     }
 
-    // example
+    // 测试玩家在前方有一个箱子时能否正常移动
     @Test
-    public void testPlayerMove29() {
+    public void testPlayerMoveBox1() {
         int[][] expectMap = new int[][]{
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -135,8 +142,35 @@ public class SokobanTest {
         Assert.assertEquals(1, sokobanGame.moveTimes);
     }
 
+    // 测试玩家在前方有两个箱子时能否正常移动
     @Test
-    public void testPlayerMove30() {
+    public void testPlayerMoveBox2() {
+        int[][] expectMap = new int[][]{
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 1, 1, 3, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 4, 3, 3, 0, 1, 0, 0, 0},
+                {0, 0, 0, 1, 0, 2, 2, 1, 0, 3, 0, 1, 1, 0, 0, 0},
+                {0, 0, 0, 1, 1, 2, 2, 1, 0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        sokobanGame.selectLevel(2);
+        IntStream.range(0, 4).forEach(_ -> sokobanGame.go("right"));
+        Assert.assertTrue(compareMap(expectMap, sokobanGame.curMap));
+        Assert.assertEquals(3, sokobanGame.moveTimes);
+    }
+
+    //    测试玩家前面有墙时能否正常移动
+    @Test
+    public void testPlayerMoveWall() {
         int[][] expectMap = new int[][]{
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -160,8 +194,58 @@ public class SokobanTest {
         Assert.assertEquals(0, sokobanGame.moveTimes);
     }
 
+    //    测试玩家能否会移动到地图边界之外，代码缺陷
     @Test
-    public void testSuccess() {
+    public void testPlayerMoveOut() {
+        int[][] expectMap = new int[][]{
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 3, 0, 3, 2, 1, 0, 0, 0, 0},
+                {3, 4, 0, 0, 0, 2, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 1, 3, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        sokobanGame.selectLevel(0);
+        sokobanGame.curMap[8][4] = 0;
+        IntStream.range(0, 8).forEach(_ -> sokobanGame.go("left"));
+        Assert.assertTrue(compareMap(expectMap, sokobanGame.curMap));
+        Assert.assertEquals(0, sokobanGame.moveTimes);
+    }
+
+    //测试超出最大步数是否失败
+    @Test
+    public void testFailure() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        sokobanGame.selectLevel(0);
+        sokobanGame.moveTimes = 99;
+        sokobanGame.go("left");
+
+        System.setOut(originalOut);
+//        输出游戏结束
+        Assert.assertTrue(outputStream.toString().contains("ゲームオーバー"));
+//        重新开始本局游戏
+        Assert.assertTrue(compareMap(sokobanGame.curLevel, sokobanGame.curMap));
+    }
+
+    //测试完成第一关
+    @Test
+    public void testSuccess1() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
         sokobanGame.selectLevel(0);
         sokobanGame.curMap = new int[][]{
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -171,8 +255,8 @@ public class SokobanTest {
                 {0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0},
-                {0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 3, 1, 0, 0, 0, 0},
-                {0, 0, 0, 0, 1, 3, 0, 0, 4, 1, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 4, 3, 2, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 3, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
                 {0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 1, 3, 1, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
@@ -180,26 +264,150 @@ public class SokobanTest {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-        Assert.assertTrue(sokobanGame.checkFinish());
-
+        sokobanGame.searchPosition(sokobanGame.curMap);
+        sokobanGame.go("right");
+        System.setOut(originalOut);
+        Assert.assertTrue(outputStream.toString().contains("恭喜过关！！"));
+        Assert.assertEquals(1, sokobanGame.iCurLevel);
     }
 
-
+    //测试完成第五十关
     @Test
-    public void testGameGuide() {
+    public void testSuccess50() {
         PrintStream originalOut = System.out;
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
+
+        sokobanGame.selectLevel(49);
+
+        sokobanGame.curMap = new int[][]{
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 1, 1, 0, 1, 3, 1, 3, 1, 3, 1, 0, 0, 1, 0, 0},
+                {1, 1, 1, 3, 3, 3, 3, 3, 3, 2, 3, 4, 0, 1, 1, 0},
+                {0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        sokobanGame.searchPosition(sokobanGame.curMap);
+        sokobanGame.go("left");
+        System.setOut(originalOut);
+        Assert.assertTrue(outputStream.toString().contains("恭喜过关！！"));
+        Assert.assertEquals(50, sokobanGame.iCurLevel);
+    }
+
+    //测试完成第一百关
+    @Test
+    public void testSuccess100() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        sokobanGame.selectLevel(99);
+
+        sokobanGame.curMap = new int[][]{
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1},
+                {0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1},
+                {0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1},
+                {0, 0, 1, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 1, 0},
+                {0, 0, 1, 0, 3, 3, 0, 0, 0, 3, 3, 1, 1, 1, 0, 0},
+                {0, 0, 0, 1, 3, 3, 3, 3, 3, 2, 1, 1, 1, 1, 1, 0},
+                {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        sokobanGame.searchPosition(sokobanGame.curMap);
+        sokobanGame.go("down");
+        System.setOut(originalOut);
+        Assert.assertTrue(outputStream.toString().contains("恭喜过关！！"));
+    }
+
+    //第一关测试输出指引
+    @Test
+    public void testGameGuide1() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
         sokobanGame.selectLevel(0);
         sokobanGame.action = 'p';
         sokobanGame.execute();
+
         System.setOut(originalOut);
-        Assert.assertTrue(outputStream.toString().contains("\n\uD83D\uDC4B这是一个推箱子小游戏，游戏目标是将\uD83D\uDCE6推到⭐处。\r\n\u001B[34ma\u001B[0m左移，\u001B[34md\u001B[0m右移，\u001B[34mw\u001B[0m上移，\u001B[34ms\u001B[0m下移，\u001B[34mr\u001B[0m撤销，\u001B[34me\u001B[0m选择关卡，\u001B[34mp\u001B[0m输出本条消息\n"));
+        Assert.assertTrue(outputStream.toString().contains("\n\uD83D\uDC4B这是一个推箱子小游戏，游戏目标是将\uD83D\uDCE6推到⭐处。\r\n\u001B[34ma\u001B[0m左移，\u001B[34md\u001B[0m右移，\u001B[34mw\u001B[0m上移，\u001B[34ms\u001B[0m下移，\u001B[34mr\u001B[0m撤销，\u001B[34me\u001B[0m选择关卡，\u001B[34mp\u001B[0m输出本条消息，\u001B[34mt\u001B[0m重新开始\n"));
     }
 
+    //第五十关测试输出指引
     @Test
-    public void testReplay() {
+    public void testGameGuide50() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        sokobanGame.selectLevel(49);
+        sokobanGame.action = 'p';
+        sokobanGame.execute();
+
+        System.setOut(originalOut);
+        Assert.assertTrue(outputStream.toString().contains("\n\uD83D\uDC4B这是一个推箱子小游戏，游戏目标是将\uD83D\uDCE6推到⭐处。\r\n\u001B[34ma\u001B[0m左移，\u001B[34md\u001B[0m右移，\u001B[34mw\u001B[0m上移，\u001B[34ms\u001B[0m下移，\u001B[34mr\u001B[0m撤销，\u001B[34me\u001B[0m选择关卡，\u001B[34mp\u001B[0m输出本条消息，\u001B[34mt\u001B[0m重新开始\n"));
+    }
+
+    //第一百关测试输出指引
+    @Test
+    public void testGameGuide100() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        sokobanGame.selectLevel(99);
+        sokobanGame.action = 'p';
+        sokobanGame.execute();
+
+        System.setOut(originalOut);
+        Assert.assertTrue(outputStream.toString().contains("\n\uD83D\uDC4B这是一个推箱子小游戏，游戏目标是将\uD83D\uDCE6推到⭐处。\r\n\u001B[34ma\u001B[0m左移，\u001B[34md\u001B[0m右移，\u001B[34mw\u001B[0m上移，\u001B[34ms\u001B[0m下移，\u001B[34mr\u001B[0m撤销，\u001B[34me\u001B[0m选择关卡，\u001B[34mp\u001B[0m输出本条消息，\u001B[34mt\u001B[0m重新开始\n"));
+    }
+
+    //第一关重玩
+    @Test
+    public void testReplay1() {
         sokobanGame.selectLevel(0);
+        sokobanGame.action = 'a';
+        sokobanGame.execute();
+        sokobanGame.action = 't';
+        sokobanGame.execute();
+        Assert.assertTrue(compareMap(SokobanGame.levels[sokobanGame.iCurLevel], sokobanGame.curMap));
+        Assert.assertEquals(0, sokobanGame.moveTimes);
+    }
+
+    //第五十关重玩
+    @Test
+    public void testReplay50() {
+        sokobanGame.selectLevel(49);
+        sokobanGame.action = 'a';
+        sokobanGame.execute();
+        sokobanGame.action = 't';
+        sokobanGame.execute();
+        Assert.assertTrue(compareMap(SokobanGame.levels[sokobanGame.iCurLevel], sokobanGame.curMap));
+        Assert.assertEquals(0, sokobanGame.moveTimes);
+    }
+
+    //第一百关重玩
+    @Test
+    public void testReplay100() {
+        sokobanGame.selectLevel(99);
         sokobanGame.action = 'a';
         sokobanGame.execute();
         sokobanGame.action = 't';
